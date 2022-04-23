2e7edc67e1ffe2cd18748e75d1525c3132ddd43a
chansooo <kcs05008@gmail.com>
2022-04-24 00:41:08 +0900

    4256

---
+++ BOJ/4256.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define f(i,l,r) for(int i=l;i<=r;++i)
+using namespace std;
+
+int a[1001], b[1001];
+
+void sol(int i, int l, int r){
+	if(l>r) return;
+	if(l == r){
+		cout << a[i] << ' ';
+		return;
+	}
+	
+	int tmp = b[a[i]];
+	sol(i+1, l, tmp-1);
+	sol(i+tmp-l+1, tmp+1, r);
+	cout << a[i] << ' ';
+}
+
+int main(){
+	int t; cin >> t;
+	while(t--){
+		int n; cin >> n;
+		f(i, 1, n)cin >> a[i];
+		f(i, 1, n){
+			int tmp; cin >> tmp;
+			b[tmp] = i;
+		}
+		sol(1, 1, n);
+		cout << endl;
+	}
+	
+	int dummy;
+	cin >> dummy;
+	return 0;
+}

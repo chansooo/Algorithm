5f20febf09418ace7ba6f81405e95e98a61b7e2c
chansooo <kcs05008@gmail.com>
2022-04-28 20:49:49 +0900

    1753

---
+++ BOJ/1753.py
@@ -0,0 +1,34 @@
+import sys
+import heapq
+
+
+V, E = map(int, sys.stdin.readline().split())
+K = int(input())
+
+graph = [[] for _ in range(V+1)]
+dist = [float('inf')] * (V+1)
+dist[K] = 0
+
+for _ in range(E):
+    u, v, w = map(int, sys.stdin.readline().split())
+    graph[u].append((v, w))
+
+pq = []
+
+heapq.heappush(pq, (0, K))
+
+while(pq):
+    curDist, curNode = heapq.heappop(pq)
+    if dist[curNode] < curDist:
+        continue
+    for destNode, destDist in graph[curNode]:
+        d = curDist + destDist
+        if dist[destNode] > d:
+            dist[destNode] = d
+            heapq.heappush(pq, (d, destNode))
+
+for i in dist:
+    if i == float('inf'):
+        print("INF")
+    else:
+        print(i)

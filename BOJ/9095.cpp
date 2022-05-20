e88b3fdc909335fe0232fcbcd0d6510773ea753b
chansooo <kcs05008@gmail.com>
2022-05-20 15:37:30 +0900

    Create 9095.cpp

---
+++ BOJ/9095.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int dp[1000] = {};
+int n, k;
+int DP(int j);
+
+int main()
+{
+
+
+	cin >> n;
+	dp[0] = 1;
+	dp[1] = 1;
+	dp[2] = 2;
+	int j;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> j;
+		cout << DP(j) << endl;
+	}
+}
+
+int DP(int j)
+{
+	for (int i = 3; i <= j; ++i)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]);
+		dp[j] = dp[i];
+	}
+	return dp[j];
+}

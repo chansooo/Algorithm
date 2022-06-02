3d74723b6a0a5a67cf5b9ee440ca9fd3d3c62aea
chansooo <kcs05008@gmail.com>
2022-06-02 21:32:44 +0900

    Update 4396.py

--- BOJ/Implementation/4396.py
+++ BOJ/Implementation/4396.py
@@ -1,18 +1,42 @@
-
 n = int(input())
 
-map = []
-mine = []
+map = list(input() for _ in range(n))
+seen = list(input() for _ in range(n))
+answer = [['.'] * n for _ in range(n)]
 
+dx = [-1, -1, -1, 0, 1, 1, 1, 0]
+dy = [-1, 0, 1, 1, 1, 0, -1, -1]
+
+def findbomb():
+    for i in range(n):
+        for j in range(n):
+
+            if map[i][j] == '.' and seen[i][j] == 'x':
+                bomb = 0
+                for k in range(8):
+                    nx = i + dx[k]
+                    ny = j + dy[k]
+
+                    if nx < 0 or nx >= n or ny < 0 or ny >= n: # 리스트 좌표 벗어났을때
+                        continue
+
+                    if map[nx][ny] == '*':
+                        bomb += 1
+                answer[i][j] = bomb
+
+            if map[i][j] == '*' and seen[i][j] == 'x':
+                show_bomb()
+
+
+def show_bomb():
+    for i in range(n):
+        for j in range(n):
+            if map[i][j] == '*':
+                answer[i][j] = '*'
+
+
+findbomb()
 for i in range(n):
-    map.append(list(input().split()))
-
-for i in range(n):
-    mine.append(list(input().split()))
-
-print(map)
-
-# for i in range(n):
-#     for j in range(n):
-#         if map[i][j] == '*':
-#             mine[i][j] = 
+    for j in range(n):
+        print(answer[i][j], end='')
+    print()

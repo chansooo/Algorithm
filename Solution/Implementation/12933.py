a7412b74d6a655ae52792a6611fb92fd6225e677
chansooo <kcs05008@gmail.com>
2022-06-27 11:39:53 +0900

    12933

--- Solution/Implementation/12933.py
+++ Solution/Implementation/12933.py
@@ -1,16 +1,33 @@
-from tokenize import String
-
-
-
 sound = input()
-visited = [0] * len(sound)
+visited = [False] * len(sound)
 duck_sound = "quack"
-print(sound[2])
 
-for i in range(5):
-    if(sound[i] == 'q'):
-        if(visited[i] == False):
+count =0
+
+if len(sound) % 5 != 0:
+    print(-1)
+    exit()
+
+def solve(start):
+    global count
+    j =0
+    first = True
+    for i in range(start, len(sound)):
+        if sound[i] == duck_sound[j] and not visited[i]:
             visited[i] = True
-            for j in range(i, len(sound)):
-                if(sound[j] == 'u'):
-                    visited[j] 
+            if sound[i] == 'k':
+                if first:
+                    count += 1
+                    first = False
+                j =0
+                continue
+            j += 1
+            
+for i in range(len(sound)):
+    if sound[i] == 'q' and not visited[i]:
+        solve(i)
+
+if not all(visited) or count == 0:
+    print(-1)
+else:
+    print(count)

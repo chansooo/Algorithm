c1e370bf4c2ebc6ac54a8dac05820d7f29df8fe3
chansooo <kcs05008@gmail.com>
2022-07-02 00:44:13 +0900

    Update 17413.py

--- Solution/Implementation/17413.py
+++ Solution/Implementation/17413.py
@@ -1,69 +1,45 @@
-# from collections import deque
+from collections import deque
 
 
-# S = input()
-# length = len(S)
-# flag = False
-# result = deque()
-# temp = deque()
-# for i in range(length):
-#     temp.append(S[i])
-#     if S[i] == '<':
-#         flag = True
-#         continue
-#     if S[i] == '>':
-#         flag = False
-#         result.append(temp)
-#         temp.clear()
-#         continue
-#     if flag == False and S[i] == ' ':
-#         temp.pop()
-#         for _ in range(len(temp)):
-#             result.append(temp.pop())
-#         result.append(' ')  
+S = input()
+length = len(S)
+flag = False
+result = deque()
+temp = deque()
+
+for i in range(length):
+    temp.append(S[i])
+    #print(S[i])
+    if S[i] == '<':
+        if len(temp) != 0:
+            temp.pop()
+            for _ in range(len(temp)):
+                result.append(temp.pop())
+            temp.append('<')
+        flag = True
+        continue
+    if S[i] == '>':
+        flag = False
+        for _ in range(len(temp)):
+            result.append(temp.popleft())
+        temp.clear()
+        continue
+    if flag == False and S[i] == ' ':
+        temp.pop()
+        for _ in range(len(temp)):
+            result.append(temp.pop())
+        result.append(' ')
+    #print(temp)
         
-# [::-1]
-# print(*result)
+if len(temp) != 0:
+    if flag:
+        for _ in range(len(temp)):
+            result.append(temp.popleft())
+    else:
+        for _ in range(len(temp)):
+            result.append(temp.pop())
 
+print(''.join(result))
 
-def solution():
-    answer = []
-    str = input()
+#print(*result)
 
-    temp = []
-    not_temp = []
-    flag = False
-
-    print(str.split("<"))
-
-    # for s in range(len(str)-1):
-    #     if str[s] == "<":
-    #         flag = True
-            
-    #     elif str[s] == ">":
-    #         flag = False
-            
-    #     if flag == True:
-    #         temp.append(str[s+1])
-    #     else:
-    #         pass
-        
-        
-    #     if str[s] == ">":
-    #         not_temp.append(str[s+1])
-            
-    
-    # temp = "".join(temp)
-    # print(temp.split(">"))
-    
-    
-    # for s in str:
-    #     if s[0] == '<':
-    #         answer.append(s)
-    #     else:
-    #         answer.append(s[::-1])
-    
-    # print(answer)
-
-solution()
-    

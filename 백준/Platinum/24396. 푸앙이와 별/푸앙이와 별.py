d38cbabdcde212f5ad1acc81731b9a0a0208ae8c
chansooo <89574881+chansooo@users.noreply.github.com>
2023-01-15 01:33:10 +0900

    [Platinum V] Title: 푸앙이와 별, Time: 872 ms, Memory: 262860 KB -BaekjoonHub

--- 백준/Platinum/24396. 푸앙이와 별/푸앙이와 별.py
+++ 백준/Platinum/24396. 푸앙이와 별/푸앙이와 별.py
@@ -2,27 +2,27 @@ import sys
 from collections import deque
 
 def bfs(n,r):
-    que = deque()
-    que.append(r)
+    q = deque()
+    q.append(r)
     visited = set(range(1,n+1))
     visited.remove(r)
-    path[r] = 0
-    while que:
-        cv = que.popleft()
-        aj[cv] = set(aj[cv])
-        for i in visited - aj[cv]:
-            que.append(i)
+    answer[r] = 0
+    while q:
+        cur_node = q.popleft()
+        cut[cur_node] = set(cut[cur_node])
+        for i in visited - cut[cur_node]:
+            q.append(i)
             visited.remove(i)
-            path[i] = path[cv]+1
+            answer[i] = answer[cur_node]+1
                 
 n,m = map(int,input().split())
-aj = [[j]for j in range(n+1)]
-path = [-1 for _ in range(n+1)]
+cut = [[j]for j in range(n+1)]
+answer = [-1 for _ in range(n+1)]
 for i in range(m):
     u,v = map(int,sys.stdin.readline().split())
-    aj[u].append(v)
-    aj[v].append(u)
+    cut[u].append(v)
+    cut[v].append(u)
 bfs(n,1)
 
 for i in range(1,n+1):
-    print(path[i])
+    print(answer[i])

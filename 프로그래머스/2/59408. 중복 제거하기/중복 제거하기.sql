50270cae5e5dcfcdbe2322a788f73da546b64f3b
chansooo <89574881+chansooo@users.noreply.github.com>
2023-10-17 22:52:17 +0900

    [level 2] Title: 중복 제거하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59408. 중복 제거하기/중복 제거하기.sql
@@ -0,0 +1,11 @@
+SELECT 
+    COUNT(*) AS count
+FROM 
+    (SELECT 
+        NAME 
+     FROM 
+        ANIMAL_INS 
+     WHERE 
+        NAME IS NOT NULL 
+     GROUP BY 
+        NAME) AS UniqueNames;

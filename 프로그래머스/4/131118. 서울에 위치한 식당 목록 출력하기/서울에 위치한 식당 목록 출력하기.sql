cc5007000ff146be457d2187d5fdd7011ce827a2
chansooo <89574881+chansooo@users.noreply.github.com>
2023-10-26 09:59:03 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,8 +1,17 @@
--- 코드를 입력하세요
-SELECT r.rest_id REST_ID, i.rest_name REST_NAME, i.food_type FOOD_TYPE, i.favorites FAVORITES, i.address ADDRESS, ROUND(AVG(r.review_score), 2) AS SCORE
-from REST_REVIEW r
-join REST_INFO i on i.rest_id = r.rest_id
-where i.address like '서울%'
-group by i.REST_ID
+select i.REST_ID, i.REST_NAME ,i.FOOD_TYPE, i.FAVORITES, i.ADDRESS, ROUND(AVG(r.review_score), 2) score
+from rest_info i, rest_review r
+where i.rest_id = r.rest_id and
+    i.address like '서울%'
+group by i.rest_id
 order by score desc, i.favorites desc;
 
+
+
+
+# -- 코드를 입력하세요
+# SELECT r.rest_id REST_ID, i.rest_name REST_NAME, i.food_type FOOD_TYPE, i.favorites FAVORITES, i.address ADDRESS, ROUND(AVG(r.review_score), 2) AS SCORE
+# from REST_REVIEW r
+# join REST_INFO i on i.rest_id = r.rest_id
+# where i.address like '서울%'
+# group by i.REST_ID
+# order by score desc, i.favorites desc;
